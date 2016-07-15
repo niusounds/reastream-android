cdce87a7258da7c5700ad2f9933c61cd188e6b3e
Yuya Matsuo <matsuo@eje-c.com>
2016-07-15 18:12:08 +0900

    Mono audio data playing support

--- libreastream/src/main/java/com/eje_c/libreastream/AudioTrackSink.java
+++ libreastream/src/main/java/com/eje_c/libreastream/AudioTrackSink.java
@@ -8,7 +8,7 @@ public class AudioTrackSink implements AutoCloseable, ReaStreamReceiverService.O
 
     private static final int FLOAT_BYTE_SIZE = Float.SIZE / Byte.SIZE;
     private final AudioTrack track;
-    private final float[] interleavedSamples = new float[ReaStreamPacket.MAX_BLOCK_LENGTH / ReaStreamPacket.PER_SAMPLE_BYTES];
+    private float[] convertedSamples;
 
     public AudioTrackSink(int sampleRate) {
         int bufferSize = Math.max(
@@ -45,8 +45,40 @@ public class AudioTrackSink implements AutoCloseable, ReaStreamReceiverService.O
     @Override
     public void onReceive(ReaStreamPacket packet) {
         if (packet.isAudioData()) {
-            packet.getInterleavedAudioData(interleavedSamples);
-            track.write(interleavedSamples, 0, packet.blockLength / ReaStreamPacket.PER_SAMPLE_BYTES, AudioTrack.WRITE_NON_BLOCKING);
+
+            final int sizeInFloats = packet.blockLength / ReaStreamPacket.PER_SAMPLE_BYTES;
+
+            if (packet.channels == 2) {
+
+                // Interleave samples
+                // [left-s1, left-s2, left-s3, ..., left-sN, right-s1, right-s2, right-s3, ..., right-sN]
+                // -> [left-s1, right-s1, left-s2, right-s2, left-s3, right-s3, ..., left-sN, right-sN]
+
+                if (convertedSamples == null || convertedSamples.length < sizeInFloats) {
+                    convertedSamples = new float[sizeInFloats];
+                }
+
+                packet.getInterleavedAudioData(convertedSamples);
+                track.write(convertedSamples, 0, sizeInFloats, AudioTrack.WRITE_NON_BLOCKING);
+
+            } else if (packet.channels == 1) {
+
+                // Convert mono -> stereo
+                // [s1, s2, s3, ...] -> [left-s1, right-s1, left-s2, right-s2, left-s3, right-s3, ...]
+                final float[] audioData = packet.audioData;
+                final int audioDataLentgh = audioData.length;
+
+                if (convertedSamples == null || convertedSamples.length < audioDataLentgh * 2) {
+                    convertedSamples = new float[audioDataLentgh * 2];
+                }
+
+                for (int i = 0; i < audioDataLentgh; i++) {
+                    final int baseIndex = i * 2;
+                    convertedSamples[baseIndex] = convertedSamples[baseIndex + 1] = audioData[i];
+                }
+
+                track.write(convertedSamples, 0, sizeInFloats * 2, AudioTrack.WRITE_NON_BLOCKING);
+            }
         }
     }
 }
